4a2a3c04c8d1ad9a0d356b814996720c7e9a4159
lukasg18 <lukas.gomes2010@gmail.com>
2017-12-16 12:23:45 -0200

    apresentacao info educacao

--- www/app/core/app.js
+++ www/app/core/app.js
@@ -70,5 +70,5 @@ angular.module('questionarioApp', ['ionic'])
         })
 ;
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/login');
+    $urlRouterProvider.otherwise('/app/questionarios');
 });
